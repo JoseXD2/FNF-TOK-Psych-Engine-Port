8c82948e47ee704d145a3219e7a2c0a939bebfc5
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-04 11:45:44 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -37,16 +37,34 @@ class OptionsState extends MusicBeatState
 	function openSelectedSubstate(label:String) {
 		switch(label) {
 			case 'Note Colors':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.NotesSubState());
 			case 'Controls':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.ControlsSubState());
 			case 'Graphics':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.GraphicsSettingsSubState());
 			case 'Visuals and UI':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.VisualsUISubState());
 			case 'Languages':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.LanguageSubState());
 			case 'Gameplay':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.GameplaySettingsSubState());
 			case 'Adjust Delay and Combo':
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
@@ -89,6 +107,11 @@ class OptionsState extends MusicBeatState
 		changeSelection();
 		ClientPrefs.saveSettings();
 
+		#if android
+		addVirtualPad(UP_DOWN, A_B_C);
+		virtualPad.y = -24;
+		#end
+			
 		super.create();
 	}
 
@@ -115,6 +138,15 @@ class OptionsState extends MusicBeatState
 		if (controls.ACCEPT) {
 			openSelectedSubstate(options[curSelected]);
 		}
+		
+		#if android
+		if (virtualPad.buttonC.justPressed) {
+			#if android
+			removeVirtualPad();
+			#end
+			openSubState(new android.AndroidControlsSubState());
+		}
+		#end
 	}
 	
 	function changeSelection(change:Int = 0) {
