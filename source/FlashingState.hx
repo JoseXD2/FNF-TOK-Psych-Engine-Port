3a4d560816b9972ff54dec7d159979be83fd5667
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-04 11:33:47 -0300

    Update FlashingState.hx

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -34,6 +34,10 @@ class FlashingState extends MusicBeatState
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
+		
+		#if android
+		addVirtualPad(UP_DOWN, A_B);
+		#end
 	}
 
 	override function update(elapsed:Float)
