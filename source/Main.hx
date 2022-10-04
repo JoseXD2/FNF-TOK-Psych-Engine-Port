957d59a2e9f4d80ac7dde6ceef64b4af02798a7c
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-04 12:05:02 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -10,8 +10,9 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
+#if windows 
 import GameJolt;
-
+#end
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
